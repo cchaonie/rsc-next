1ee9b77e6db709ca42d942f01c0187db7e2ec696
Chris Nie <cnie@travix.com>
2024-02-20 16:00:53 +0800

    move components here

--- src/app/blogs/route.js
+++ src/app/blogs/route.js
@@ -1,3 +1,14 @@
+import path from 'node:path';
+import {
+  db,
+  insertNote,
+  editNote,
+  findNote,
+  deleteNote,
+} from '../../utils/db.js';
+
+const NOTES_PATH = path.resolve(__dirname, '../notes');
+
 export async function GET(request) {
   return Response.json({ message: 'Hello from the blog route!' });
 }
